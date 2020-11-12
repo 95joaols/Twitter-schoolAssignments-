03c83c88ae32f3fc102c855d975598bbb20bbd4e
Joacim Olsson <Joacim.olsson95@gmail.com>
2020-11-12 12:21:43 +0100

    Description

--- Repository/Enum/JoinTable.cs
+++ Repository/Enum/JoinTable.cs
@@ -1,8 +1,11 @@
 using System;
+using System.ComponentModel;
 
 namespace Repository.Enum
 {
     public enum JoinTable
     {
+        [Description("Exit the program")]
+        aJoinTable
     }
 }
