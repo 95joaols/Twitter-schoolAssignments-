09603f53f85878936d6904b8f0d1e3b63126ddcd
Joacim Olsson <Joacim.olsson95@gmail.com>
2020-11-12 09:58:17 +0100

    ISeekable

--- Repository/ISeekable.cs
+++ Repository/ISeekable.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 namespace Repository
 {
     public interface ISeekable
     {
+        T GetSingel<T>(string Selekt, Table table, IDictionary<string,string> Where);
+        IEnumerable<T> GetMenySingelTabel<T>(string Selekt, Table table, IDictionary<string, string> Where);
     }
 }
