4f8d2da2e34a5bd354bd09b43f32762107ff3ff1
Joacim Olsson <Joacim.olsson95@gmail.com>
2020-11-14 13:06:06 +0100

    Add tweet

---
+++ Twitter.Blazor.Server/Pages/TweetsOverview.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TwitterCore;
+
+namespace Twitter.Blazor.Server.Pages
+{
+    public partial class TweetsOverview
+    {
+        public IEnumerable<Tweet> Tweets { get; set; }
+
+        protected override void OnInitialized()
+        {
+            TweetManager tweetManager = new TweetManager();
+            Tweets = tweetManager.GetTweets(50);
+        }
+    }
+}
