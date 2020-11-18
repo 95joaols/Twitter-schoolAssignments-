353990608d3421e60ca756750c18e8d46b5919d7
Marcus Johansson <marcus.s.johansson@hotmail.com>
2020-11-18 10:10:57 +0100

    ändrade variabelnamnet från test till searchMenu från tidigare commit

--- Grupparbete/Program.cs
+++ Grupparbete/Program.cs
@@ -156,9 +156,9 @@ namespace Grupparbete
                 Console.WriteLine("User Id: {0}, Username: {1}, Biography: {2}, Message: {3}, CreateDate: {4}, IdTweet: {5}", x.Id, x.Username, x.Biography, x.Message, x.CreateDate, x.IdTweet);
             }
 
-            bool test = true;
+            bool searchMenu = true;
 
-            while (test)
+            while (searchMenu)
             {
                 Console.WriteLine(Environment.NewLine + "[1] Sort by users");
                 Console.WriteLine("[2] Sort by tweets");
@@ -248,7 +248,7 @@ namespace Grupparbete
                         {
                             // TweetMenu(new User { Id = 777, Username = "Ghost" });                 // TODO: Sneaky solution! :)
                             // break;
-                            test = false;
+                            searchMenu = false;
                             break;
                         }
 
