7b27835e6caf4b054a9f7fe49071e79ef4804550
Joacim Olsson <95joaols@skola.boras.se>
2020-11-19 11:29:05 +0100

    stating that you can tweet

---
+++ Twitter.Blazor.Server/Components/AddtweetDialog.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Twitter.Blazor.Server.Data;
+using TwitterCore;
+
+namespace Twitter.Blazor.Server.Components
+{
+    public partial class AddtweetDialog
+    {
+        public User User { get; set; } = new User();
+
+        public bool ShowDialog { get; set; }
+
+        public bool HasError { get; set; }
+        public string Messege { get; set; }
+
+        [Inject]
+        private IDataAccess DataAccess { get; set; }
+
+        public void Show()
+        {
+            ShowDialog = true;
+            StateHasChanged();
+        }
+
+        public void Close()
+        {
+            ShowDialog = false;
+            StateHasChanged();
+        }
+    }
+}
